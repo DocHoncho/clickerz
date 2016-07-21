7626d6a9e4ada8ed6e97da3cbf813e69d759dde2
Joel Madigan <dochoncho@gmail.com>
2016-07-21 04:48:22 -0400

    Rename Game/Environment Game/Constants

--- src/Game/Constants.js
+++ src/Game/Constants.js
@@ -4,10 +4,13 @@
  *
  * I do.
  */
-const Environment = {
+
+const moment = require('moment');
+
+const UniversalConstants = {
   worldEndDate: moment({year: 2092, month: 4, day: 1, hour: 9, minute: 47, second: 32}),
   timeSinceWorldEnd: moment.duration({months: 4, days: 7, hours: -4, minute: 13, second: 20}),
   meaningOfLife: 42,
 }
 
-module.exports = Environment;
+module.exports = UniversalConstants;
