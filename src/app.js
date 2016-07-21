6e45e8b8540367a17382baa5cde2f79395e6dd23
Joel Madigan <dochoncho@gmail.com>
2016-07-21 04:48:53 -0400

    * Removed more non-browser friendly ES6isms

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ window.jQuery = window.$ = require('jquery');
 require('bootstrap-loader');
 require('font-awesome/css/font-awesome.css');
 require('./scss/main.scss');
-import App from './App/App';
+
+const App = require('./App/App');
 
 var app = new App();
 $('#root').html(app.render());
